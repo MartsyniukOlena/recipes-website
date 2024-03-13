f23c478eadcaa8b90d7f4124633b1b5497e8a13d
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-13 11:05:48 +0000

    Add recipe detail view

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('recipes/', views.RecipeList.as_view(), name='recipes_list'),
+    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -7,3 +7,27 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1)
     template_name = "recipes/recipe_list.html"
     paginate_by = 3
+
+
+def recipe_detail(request, slug):
+    """
+    Display an individual :model:`recipes.Recipe`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`recipes.Recipe`.
+
+    **Template:**
+
+    :template:`recipes/recipe_detail.html`
+    """
+
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "recipes/recipe_detail.html",
+        {"recipe": recipe},
+    )
