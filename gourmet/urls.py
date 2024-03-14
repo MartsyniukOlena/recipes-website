bf9e1ea93149634424980cd52a1d48b734b37c90
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-14 11:37:19 +0000

    Create Club app and model, add urls

--- gourmet/urls.py
+++ gourmet/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+     path("club/", include("club.urls"), name="club-urls"),
     path("", include("recipes.urls"), name="recipes-urls"),
     path('', include('recipes.urls')),
 ]
