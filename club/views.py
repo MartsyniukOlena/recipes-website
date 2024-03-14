9990051c6e77b7e3c1623da0c9b1353d4fbc0e35
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-14 11:40:36 +0000

    Add views for Club

--- club/views.py
+++ club/views.py
@@ -1,2 +1,15 @@
 from django.shortcuts import render
+from .models import Club
 
+# Create your views here.
+def club_gourmet(request):
+    """
+    Renders the Gourmet Club page
+    """
+    club = Club.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "club/club.html",
+        {"club": club},
+    )
