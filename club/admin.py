aba0292face78f796f87fc53e44a83f4a030a588
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-14 11:39:20 +0000

    Register Club model, add summernote

--- club/admin.py
+++ club/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import Club
+from django_summernote.admin import SummernoteModelAdmin
 
+# Register your models here.
+
+@admin.register(Club)
+class ClubAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content', 'venue', 'agenda')
