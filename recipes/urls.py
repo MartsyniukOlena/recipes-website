9b5e22955271fd233eb080dbac3e8af9fd63d292
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-15 14:47:39 +0000

    added comment CRUD

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('add/', views.add_recipe, name='add_recipe'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('recipes/', views.RecipeList.as_view(), name='recipes_list'),
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
     path('', views.index, name='index'),
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
 from django.contrib import messages
 from django.utils.text import slugify
-from .models import Recipe
+from django.http import HttpResponseRedirect
+from .models import Recipe, Comment
 from .forms import CommentForm, RecipeForm
 
 # Create your views here.
@@ -92,3 +93,61 @@ def add_recipe(request):
     else:
         form = RecipeForm()
     return render(request, 'recipes/add_recipe.html', {'form': form})
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+    """
+    if request.method == "POST":
+
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    """
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
