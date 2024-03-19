7c0b42648d14ed7544e2bc8b9af4b13b3bed387a
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-19 21:53:55 +0000

    Add image field to Club model, create EventForm model, update club.html page

--- club/models.py
+++ club/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Club(models.Model):
+    """
+    Stores a single text about the club
+    """
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class EventForm(models.Model):
+    """
+    Stores a single collaboration request message
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
