e3ca9501a8c5dcba1640a088143e276f2aff01fd
Olena Martsyniuk <martsyniuko@gmail.com>
2024-03-21 12:47:45 +0000

    Test recipe details page rendering and comment and recipe submission

---
+++ recipes/test_views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import TestCase
+from .forms import CommentForm
+from .models import Recipe
+import unittest
+
+class TestRecipeViews(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="myUsername",
+            password="myPassword",
+            email="test@test.com"
+        )
+        self.recipe = Recipe(title="Recipe title", author=self.user,
+                         slug="recipe-title", excerpt="Recipe excerpt",
+                         content="Recipe content", cooking_time=1, servings=2, status=1)
+        self.recipe.save()
+
+
+    def test_render_recipe_detail_page_with_comment_form(self):
+        """Verifies a single recipe page containing a comment form is returned"""
+        response = self.client.get(reverse('recipe_detail', args=['recipe-title']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Recipe title", response.content)
+        self.assertIn(b"Recipe content", response.content)
+        self.assertIsInstance(
+            response.context['comment_form'], CommentForm)
+
+
+    def test_successful_comment_submission(self):
+        """Test for posting a comment on a post"""
+        self.client.login(username="myUsername", password="myPassword")
+        post_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(
+            reverse('recipe_detail', args=['recipe-title']), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Comment submitted and awaiting approval',
+                      response.content)
+
+
+    def test_successful_recipe_submission(self):
+        self.client.login(username="myUsername", password="myPassword")
+        response = self.client.post(reverse('add_recipe'), {'title': 'New Recipe', 'content': 'Step 1: Do this, Step 2: Do that', 'cooking_time': 30,
+            'servings': 4, 'status': 1})
+        self.assertEqual(response.status_code, 302)
