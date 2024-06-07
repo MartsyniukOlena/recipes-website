6b83637ef5135b1ec02fb8275059e356f4fdeaa0
Olena Martsyniuk <martsyniuko@gmail.com>
2024-06-07 09:51:39 +0000

    Update secret key settings in settings file

--- gourmet/settings.py
+++ gourmet/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-50@hscl7^o=qlvu!c-kd7^aozo1-sn87$&4-$i(5s=%whq2u!f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
